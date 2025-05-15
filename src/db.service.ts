a7dec7bd78901c10bbcb56a2a515bc7edddc81b4
RashingPro <rashingpro@yandex.ru>
2025-05-15 18:35:49 +0300

    Fix Permissions class

--- src/db.service.ts
+++ src/db.service.ts
@@ -1,41 +1,43 @@
-import anyBase from "any-base";
 import {Injectable} from "@nestjs/common";
 import {neon, NeonQueryFunction} from "@neondatabase/serverless";
-import binToDec = BaseConverters.binToDec;
 
-
-namespace BaseConverters {
-    export const decToBin = anyBase(anyBase.DEC, anyBase.BIN);
-    export const binToDec = anyBase(anyBase.BIN, anyBase.DEC);
+function decToBinBuiltIn(n: number): string {
+    if (n < 0) throw new Error("Number should be a positive integer");
+    return (n >>> 0).toString(2);
 }
 
 export class Permissions {
-    private _permissions: number
-
-    constructor() {
-
-    }
+    private _permissions: number = 0
 
     private PermissionsMap: string[] = [
         "admin"
     ]
 
     public set(key: string, value: boolean): Permissions {
-        if (!this.PermissionsMap.includes(key)) return this;
-        const binary = BaseConverters.decToBin(this._permissions.toString())
+        if (!this.PermissionsMap.includes(key)) {
+            return this;
+        }
+        let binary = decToBinBuiltIn(this._permissions)
+        while (binary.length < this.PermissionsMap.length) {
+            binary = "0" + binary
+        }
         const index = binary.length - 1 - this.PermissionsMap.indexOf(key);
         let result = "";
-        result = binary.slice(0, index) + value ? "1" : "0"
+        result = binary.slice(0, index) + (value ? "1" : "0")
         if (index + 1 < binary.length) result += binary.slice(index + 1);
-        this._permissions = parseInt(binToDec(result));
+        this._permissions = parseInt(result, 2);
         return this;
     }
 
     public get(key: string): boolean {
         if (!this.PermissionsMap.includes(key)) return false;
-        const binary = BaseConverters.decToBin(this._permissions.toString())
+        const binary = decToBinBuiltIn(this._permissions)
         const index = binary.length - 1 - this.PermissionsMap.indexOf(key);
         return binary[index] == "1";
+    }
+
+    public getAll(): number {
+        return this._permissions;
     }
 }
 
@@ -58,14 +60,18 @@ export class DatabaseService {
         return res;
     }
 
-    async getRows(values: string, table: string, where: string, params?: any, additional_query?: string) {
+    async getRows(values: string, table: string, where: string, params?: any[], additional_query?: string) {
         const res = await this.doSql(`SELECT ${values} FROM ${table} WHERE ${where} ${additional_query}`, params)
         return res;
     }
 
-    async getSingleRow(values: string, table: string, where: string, params?: any, additional_query?: string) {
+    async getSingleRow(values: string, table: string, where: string, params?: any[], additional_query?: string) {
         const [res] = await this.getRows(values, table, where, params, additional_query);
         return res;
+    }
+
+    async insertRow(table: string, columns: string, values: string, params?: any[], additional_query?: string) {
+        await this.doSql(`INSERT INTO ${table} (${columns}) VALUES (${values})`, params)
     }
     // </core>
 
@@ -75,8 +81,9 @@ export class DatabaseService {
         return user as User;
     }
 
-    async createUser() {
-
+    async createUser(permissions: Permissions) {
+        const convertedPermissions = permissions.getAll();
+        await this.insertRow("Users", "permissions", "$1", [convertedPermissions])
     }
     // </auth>
 }
