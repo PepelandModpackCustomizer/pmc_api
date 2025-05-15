6e89d9072cff19bfa2ab04455b0f02fa94c0bee9
RashingPro <rashingpro@yandex.ru>
2025-05-15 17:07:44 +0300

    Add User interface, Permissions class, BaseConverters interface to db.service

--- src/db.service.ts
+++ src/db.service.ts
@@ -1,5 +1,48 @@
+import anyBase from "any-base";
 import {Injectable} from "@nestjs/common";
 import {neon, NeonQueryFunction} from "@neondatabase/serverless";
+import binToDec = BaseConverters.binToDec;
+
+
+namespace BaseConverters {
+    export const decToBin = anyBase(anyBase.DEC, anyBase.BIN);
+    export const binToDec = anyBase(anyBase.BIN, anyBase.DEC);
+}
+
+export class Permissions {
+    private _permissions: number
+
+    constructor() {
+
+    }
+
+    private PermissionsMap: string[] = [
+        "admin"
+    ]
+
+    public set(key: string, value: boolean): Permissions {
+        if (!this.PermissionsMap.includes(key)) return this;
+        const binary = BaseConverters.decToBin(this._permissions.toString())
+        const index = binary.length - 1 - this.PermissionsMap.indexOf(key);
+        let result = "";
+        result = binary.slice(0, index) + value ? "1" : "0"
+        if (index + 1 < binary.length) result += binary.slice(index + 1);
+        this._permissions = parseInt(binToDec(result));
+        return this;
+    }
+
+    public get(key: string): boolean {
+        if (!this.PermissionsMap.includes(key)) return false;
+        const binary = BaseConverters.decToBin(this._permissions.toString())
+        const index = binary.length - 1 - this.PermissionsMap.indexOf(key);
+        return binary[index] == "1";
+    }
+}
+
+export interface User {
+    user_id: string,
+    permissions: Permissions
+}
 
 @Injectable()
 export class DatabaseService {
@@ -29,7 +72,11 @@ export class DatabaseService {
     // <auth>
     async getUser(user_id: string, values: string = "*") {
         const user = await this.getSingleRow(values, "Users", "user_id = $1", [user_id])
-        return user;
+        return user as User;
+    }
+
+    async createUser() {
+
     }
     // </auth>
 }
